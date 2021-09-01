8ae4173dcc473e00f1bf8cf6357669ad310739f8
johnfreyes <jhonfer96@hotmail.com>
2021-08-31 23:29:19 -0500

    Add document type

--- Vehicles.API/Data/DataContext.cs
+++ Vehicles.API/Data/DataContext.cs
@@ -10,6 +10,7 @@ namespace Vehicles.API.Data
         }
 
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<DocumentType> DocumentTypes { get; set; }
         public DbSet<Procedure> Procedures { get; set; }
         public DbSet<VehicleType> VehicleTypes { get; set; }
 
@@ -17,6 +18,7 @@ namespace Vehicles.API.Data
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Brand>().HasIndex(x => x.Description).IsUnique();
+            modelBuilder.Entity<DocumentType>().HasIndex(x => x.Description).IsUnique();
             modelBuilder.Entity<Procedure>().HasIndex(x => x.Description).IsUnique();
             modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
         }
