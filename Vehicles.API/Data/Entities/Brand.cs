40733f54e49630e39e7226c58c4d5d652181469e
johnfreyes <jhonfer96@hotmail.com>
2021-08-31 23:09:57 -0500

    Add brand

--- Vehicles.API/Data/Entities/Brand.cs
+++ Vehicles.API/Data/Entities/Brand.cs
@@ -2,11 +2,11 @@
 
 namespace Vehicles.API.Data.Entities
 {
-    public class VehicleType
+    public class Brand
     {
         public int Id { get; set; }
 
-        [Display(Name = "Tipo de vehículo")]
+        [Display(Name = "Marca")]
         [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Description { get; set; }
