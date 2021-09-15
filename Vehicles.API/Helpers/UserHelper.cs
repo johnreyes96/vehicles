de0e7c19dfb84e845262960dd6823f3971efc84b
johnfreyes <jhonfer96@hotmail.com>
2021-09-15 00:42:52 -0500

    reset password and recover password user

--- Vehicles.API/Helpers/UserHelper.cs
+++ Vehicles.API/Helpers/UserHelper.cs
@@ -90,6 +90,11 @@ namespace Vehicles.API.Helpers
             return await _userManager.GenerateEmailConfirmationTokenAsync(user);
         }
 
+        public async Task<string> GeneratePasswordResetTokenAsync(User user)
+        {
+            return await _userManager.GeneratePasswordResetTokenAsync(user);
+        }
+
         public async Task<User> GetUserAsync(string email)
         {
             return await _context.Users
@@ -127,6 +132,11 @@ namespace Vehicles.API.Helpers
         public async Task LogoutAsync()
         {
             await _signInManager.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
+        {
+            return await _userManager.ResetPasswordAsync(user, token, password);
         }
 
         public async Task<IdentityResult> UpdateUserAsync(User user)
